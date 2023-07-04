c6a91f17f8fabbf096cc4aef620cb0f5e7b449f5
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-07-04 17:48:45 +0300

    Display Comments pop up with selected item's details

---
+++ src/modules/fetchComment.js
@@ -0,0 +1,35 @@
+import renderPopComment from './renderPopComment';
+import fetchPro from './fetchPro';
+import getComment from './getComments';
+
+const url =
+  'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/hqiz5X9EpndT4RnMoRZ6/comments/';
+
+const display = async (comments, id) => {
+  const commentData = await getComment(id);
+  const listComments = commentData
+    .map(
+      (comment) => `<div class="list-items"> 
+ ${comment.creation_date}  ${comment.username}: ${comment.comment}</div>`
+    )
+    .join('');
+  comments.innerHTML = '';
+  comments.insertAdjacentHTML('afterbegin', listComments);
+};
+
+const addComment = async (e, id) => {
+  e.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const comment = document.getElementById('comment').value;
+  const data = {
+    item_id: `${id}`,
+    username: name,
+    comment: comment,
+  };
+  fetchPro(url, data);
+  document.getElementById('name').value = '';
+  document.getElementById('comment').value = '';
+};
+
+export { addComment, display };
