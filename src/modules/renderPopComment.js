30a1c9a9afe7b7fc4be4a9c1febc405b02d99db7
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-07-04 12:10:44 +0300

    Fix comment popup style

--- src/modules/renderPopComment.js
+++ src/modules/renderPopComment.js
@@ -5,29 +5,31 @@ const renderPopComment = (data, id) => {
   const markUp = meals
     .map(
       (meal) => `
+      
         <div class="flex flex-col flex-center">
+        <div class="like flex-center">
+                  <p class="space">${meal.strMeal}</p>
+                </div>
           <div>
             <img class="img-thumb" src="${meal.strMealThumb}" alt="" />
             </div>
-                <div class="like flex-center">
-                  <p class="space">${meal.strMeal}</p>
-                </div>
+                
               <section class="comment-section">
                 <div class="comment-display">
-                  <h2>comment</h2>
+                  <h2>Comments</h2>
                   <div class="comment-container"></div>
                 </div>
                 <form class="comment-form flex flex-col" action="/">
                   <h2>Add a comment</h2>
-                  <input class="comment-input" type="text" id="username" />
-                  <textarea class="comment-textarea" cols="30" rows="5"></textarea>
+                  <input class="comment-input" type="text" placeholder="Your name" id="username" />
+                  <textarea class="comment-textarea" placeholder="Your comment" cols="30" rows="5"></textarea>
                   <div class="btn-submit">
                     <button class="comment-btn">Comment</button>
                   </div>
                 </form>
           </section>
       </div>
-    `,
+    `
     )
     .join('');
   model.innerHTML = '';
